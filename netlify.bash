6c7f44b1b82b5343972c1092ea2f1b9fa5d8162e
slorello89 <slorello89@gmail.com>
2022-05-18 16:48:44 -0400

    pipfile

--- netlify.bash
+++ netlify.bash
@@ -3,10 +3,4 @@
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 npm install autoprefixer
-
-sudo apt-get update
-sudo apt-get install -y python3-pip
-
-pip3 install -r requirements.txt
-
 make netlify
