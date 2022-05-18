f72895f308809fa975465f8f540dcce91183bd50
slorello89 <slorello89@gmail.com>
2022-05-18 16:23:40 -0400

    adding pat to netlify checkout

--- netlify.bash
+++ netlify.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone --recurse-submodules https://github.com/redis-stack/redis-stack-website
+git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 npm install autoprefixer
-PRIVATE_ACCESS_TOKEN=$PRIVATE_ACCESS_TOKEN REDIS_STACK_DOCS_HASH=$REDIS_STACK_DOCS_HASH make netlify up
+make netlify
