b30d1263d932afc1e660081e73a23487bf2b9539
slorello89 <slorello89@gmail.com>
2022-05-18 16:59:19 -0400

    removing hugo

--- netlify.bash
+++ netlify.bash
@@ -3,4 +3,4 @@
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 npm install autoprefixer
-make netlify up
+make netlify
