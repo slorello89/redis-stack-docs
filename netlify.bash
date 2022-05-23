5e7daa0fd6fea7a25252f1839cabe7b948a78398
slorello89 <slorello89@gmail.com>
2022-05-23 08:00:02 -0400

    removing repo

--- netlify.bash
+++ netlify.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -rf redis-stack-website
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/slorello89/redis-stack-website
 cd redis-stack-website
 npm install autoprefixer
