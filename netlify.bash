eab9f714df5af496f3e2a0cc8013c8a14a228b9e
slorello89 <slorello89@gmail.com>
2022-05-23 07:46:08 -0400

    levering env vars

--- netlify.bash
+++ netlify.bash
@@ -3,4 +3,6 @@
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 npm install autoprefixer
-make netlify
+echo $COMMIT_REF
+echo $REPOSITORY_URL
+REDIS_STACK_DOCS_SHA=$COMMIT_REF REDIS_STACK_DOCS_REMOTE=$REPOSITORY_URL make netlify
